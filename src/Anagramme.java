39c0fea9d3589b57a52591f896ee309607a8eb40
Karim Ibrahim <chillman359@gmail.com>
2021-09-13 16:51:02 -0400

    Premiere algo fixed.

--- src/Anagramme.java
+++ src/Anagramme.java
@@ -7,10 +7,8 @@ import java.util.List;
 public class Anagramme {
 
     public static boolean isAnagramme_First(String chaine1, String chaine2) {
-        if (chaine1 == null || chaine2 == null) return false;
-
-        chaine1 = chaine1.replaceAll("\\s", "");
-        chaine2 = chaine2.replaceAll("\\s", "");
+        chaine1 = chaine1.replaceAll("\s", "");
+        chaine2 = chaine2.replaceAll("\s", "");
 
         if (chaine1.length() != chaine2.length()) {
             return false;
@@ -19,10 +17,11 @@ public class Anagramme {
         for (char c1 : chaine1.toCharArray()) {
             boolean found = false;
             for (char c2 : chaine2.toCharArray()) {
-                if (c1 == c2) {
-                    chaine2 = chaine2.replaceFirst(Character.toString(c2), Character.toString(Character.MIN_VALUE));
-                    found = true;
-                    break;
+                if(!found) {
+                    if (c1 == c2) {
+                        chaine2 = chaine2.replaceFirst(Character.toString(c2), Character.toString(Character.MIN_VALUE));
+                        found = true;
+                    }
                 }
             }
             if (!found) {
@@ -67,16 +66,19 @@ public class Anagramme {
         boolean resultat2 = isAnagramme_Second("b  a  a d a", "aaabd");
         long endTime2 = System.nanoTime();
 
-        System.out.println("First algorithme : " + resultat1 + " " + (endTime1 - startTime1) + " ns");
-        System.out.println("Second algorithme : " + resultat2 + " " + (endTime2 - startTime2) + " ns");
+        System.out.println("First algorithme : " + resultat1 + " " + (double)(endTime1 - startTime1)/1000000 + " ms");
+        System.out.println("Second algorithme : " + resultat2 + " " + (double)(endTime2 - startTime2)/1000000 + " ms");
     }
 
     public static void main(String[] args) throws Exception {
 
-        testAlgorithmeSpeed();
+        //testAlgorithmeSpeed();
 
-        File dict = new File("./src/dict.txt");
+//        File mots = new File(args[0]);
+//        File dict = new File(args[1]);
+
         File mots = new File("./src/words.txt");
+        File dict = new File("./src/dict.txt");
 
         List<String> Dictionnary = new ArrayList<>();
         List<String> AllWords = new ArrayList<>();
@@ -97,25 +99,23 @@ public class Anagramme {
             AllWords.add(line);
         }
 
+        long startTime1 = System.nanoTime();
         for (String words : AllWords) {
-
             int found = 0;
-
             for (String words_in_dict : Dictionnary) {
-
-                if (words_in_dict != null && isAnagramme_Second(words, words_in_dict)) {
+                if (words_in_dict != null && isAnagramme_First(words, words_in_dict)) {
                     found++;
                 }
             }
             Anagramme_found.add(found);
-
         }
+        long endTime1 = System.nanoTime();
 
         for (int i = 0; i < AllWords.size(); i++) {
-
             System.out.println(AllWords.get(i) + " " + Anagramme_found.get(i));
-
         }
+
+        System.out.println("Second algorithme : " + (double)(endTime1 - startTime1)/1000000 + " ms");
 
     }
 
